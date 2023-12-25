1f365b17c540e8fc62d15f3db549f5f6c1adc606
hs102 <herotato108@gmail.com>
2023-12-25 23:31:33 +0300

    dknkjds

--- components/connect.php
+++ components/connect.php
@@ -1,9 +1,9 @@
 <?php
 // connection detais of the current device
-$host = "localhost:3306";
-$username = "root";
-$password = "";
-$dbname = "pollify";
+$host = "localhost";
+$username = "u515027391_pollify";
+$password = "Pollify@123";
+$dbname = "    u515027391_pollify";
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
