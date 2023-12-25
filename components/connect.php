3c0280e60edae10257351a21081cbbf493e08a98
Ali Housam AlHalaki <62831627+3li7alaki@users.noreply.github.com>
2023-12-25 23:37:22 +0300

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $username = "u515027391_pollify";
 $password = "Pollify@123";
-$dbname = "    u515027391_pollify";
+$dbname = "u515027391_pollify";
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
